3c690e1d62f006dcf8cdf2b7ee38433e6d8b9ac9
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2017-10-26 16:57:55 +0100

    Add files via upload

---
+++ calculos.java
@@ -0,0 +1,13 @@
+package pacote2;
+
+public class calculos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//System.out.printf ("%d",4+3*(15-2));
+//System.out.printf("%f",40.0/9.0);
+//System.out.printf("%d\n", 40%3);
+
+	}
+
+}
