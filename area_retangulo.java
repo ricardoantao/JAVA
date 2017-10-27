4782b5ebe5a3699742e6ec444b893c80b0d9c78f
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2017-10-27 12:06:22 +0100

    Add files via upload

---
+++ area_retangulo.java
@@ -0,0 +1,22 @@
+package area_retangulo;
+
+import java.util.Scanner;
+
+public class area_retangulo {
+
+	public static void main(String[] args) {
+		
+		 Scanner s = new Scanner(System.in);
+		 System.out.println("Qual o comprimento?");
+		 int comprimento = s.nextInt();
+		 System.out.println("Qual a largura?");
+		 int largura = s.nextInt();
+		 
+		 System.out.println(comprimento*largura);
+
+		 
+	}
+		}
+	
+
+
