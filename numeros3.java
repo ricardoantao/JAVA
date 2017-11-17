160906a01b99ae2c1ecc23709be1a67a38449044
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2017-11-17 17:11:03 +0000

    Add files via upload

--- numeros3.java
+++ numeros3.java
@@ -1,25 +1,27 @@
 package numeros3;
+
 import java.util.Scanner;
+
 public class numeros3 {
 
-	public static void main1(String[] args) {}
-
-			public static void main(String[] args) {
-				Scanner dados = new Scanner(System.in);
-				double numero1, numero2,numero3;
-				System.out.println("Qual o numero 1?");
-				numero1 = dados.nextDouble();
-				System.out.println("Qual o numero 2?");
-				numero2 = dados.nextDouble();
-				System.out.println("Qual o numero 3?");
-				numero3 = dados.nextDouble();
-				if (numero1 > numero2) 
-					System.out.println("O Maior Numero é " + numero1);
-				else if (numero2 > numero1) 
-					System.out.println("O Maior Numero é " + numero2);
-				if (numero1 < numero3 > numero2)
-					System.out.println("O Maior Numero é " + numero3);
-					else
-				System.out.print("Os numeros são iguais!");	
-			}	
-		}
+	public static void main(String[] args) 
+	{{
+Scanner Ler = new Scanner (System.in);
+double numero1,numero2,numero3;
+double max=0;
+System.out.println("Qual o 1º número?");
+numero1 = Ler.nextDouble();
+System.out.println("Qual o 2º número?");
+numero2 = Ler.nextDouble();
+System.out.println("Qual o 3º número?");
+numero3 = Ler.nextDouble();
+if (numero1>numero2)
+	max=numero1;
+	else
+		max=numero2;
+		if(max < numero3)
+			max=numero3;
+			
+System.out.println("Máximo: "+max);		
+		
+}}}
