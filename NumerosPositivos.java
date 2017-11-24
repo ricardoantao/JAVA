60902979b0d79468770a4f779cc533e5c72a320e
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2017-11-24 15:28:04 +0000

    Update NumerosPositivos.java

--- NumerosPositivos.java
+++ NumerosPositivos.java
@@ -1,15 +1,19 @@
 package Sexta;
 
+import java.util.Scanner;
+
 public class NumerosPositivos {
 
 	public static void main(String[] args) {
 	
 int n1=1;
-int i;
-
-for(i=2;i<=10;i++)
+int i, n2;
+Scanner dados = new Scanner (System.in);
+System.out.println("Até que número?");
+		n2 = dados.nextInt();
+for(i=2;i<=n2;i++)
 n1=n1+i;
 		
-System.out.println("A soma dos 10 primeiros n�meros �: "+n1);
+System.out.println("A soma dos "+n2+" primeiros números é: "+n1);
 	
 }}
