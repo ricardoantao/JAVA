6b5d79766b3cdfd7486212c6e1783246406aae86
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2017-11-24 14:56:17 +0000

    Add files via upload

---
+++ Sexta.java
@@ -0,0 +1,11 @@
+package Sexta;
+
+public class Sexta {
+
+	public static void main(String[] args) {
+	final String mensagem = "Hoje é Sexta-Feira!";
+for (int I=1; I<=5; I++)
+System.out.println(mensagem);
+	}
+
+}
