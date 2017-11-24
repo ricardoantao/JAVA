dcaeeeebf3679183e33cb339e0c61bf92b5b851a
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2017-11-24 15:15:42 +0000

    Add files via upload

---
+++ NumerosPositivos.java
@@ -0,0 +1,15 @@
+package Sexta;
+
+public class NumerosPositivos {
+
+	public static void main(String[] args) {
+	
+int n1=1;
+int i;
+
+for(i=2;i<=10;i++)
+n1=n1+i;
+		
+System.out.println("A soma dos 10 primeiros números é: "+n1);
+	
+}}
