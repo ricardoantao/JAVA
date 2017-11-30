70a87a54d171e88e2e9c85937ae9bc002718546a
ricardoantao <32168853+ricardoantao@users.noreply.github.com>
2017-11-30 17:27:08 +0000

    Add files via upload

---
+++ PrimeirasContas.java
@@ -0,0 +1,15 @@
+package leia;
+
+public class PrimeirasContas {
+
+	public static void main(String[] args) {
+		System.out.printf("%d\n", 4+3*(15-2));
+		System.out.printf("%f\n", 40.0/9.0);
+		System.out.printf("%d", 40%3);
+	}
+}
+	
+	
+	
+	
+
